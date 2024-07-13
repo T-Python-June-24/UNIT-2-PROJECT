acee75ac650e76bf77ea9e90c89e7f4affb6f25e
e7gx <abalgabo@gmail.com>
2024-07-13 06:36:20 +0300

    finished setting up firebase 🔥

--- SaudiArabia/Tourister/urls.py
+++ SaudiArabia/Tourister/urls.py
@@ -1,12 +1,9 @@
-
+# Tourister/urls.py
 from django.urls import path
 from . import views
-
 
 app_name = 'Tourister'
 
 urlpatterns = [
-    
     path('', views.home, name='home'),
-    
 ]
--- SaudiArabia/Tourister/views.py
+++ SaudiArabia/Tourister/views.py
@@ -1,9 +1,40 @@
-from django.shortcuts import render
+# views.py
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-# Create your views here.
+from .forms import DataForm
+import pyrebase
+import os
+from dotenv import load_dotenv
 
+load_dotenv()  # Load environment variables from .env file
 
+config = {
+    "apiKey": os.getenv("API_KEY"),
+    "authDomain": os.getenv("AUTH_DOMAIN"),
+    "projectId": os.getenv("PROJECT_ID"),
+    "storageBucket": os.getenv("STORAGE_BUCKET"),
+    "messagingSenderId": os.getenv("MESSAGING_SENDER_ID"),
+    "appId": os.getenv("APP_ID"),
+    "databaseURL": os.getenv("DATABASE_URL"),
+}
 
-def home(request:HttpResponse):
-    return render(request, 'tourister/home.html')
+firebase = pyrebase.initialize_app(config)
+database = firebase.database()
 
+def home(request):
+    if request.method == 'POST':
+        form = DataForm(request.POST)
+        if form.is_valid():
+            field_name = form.cleaned_data['field_name']
+            field_value = form.cleaned_data['field_value']
+            database.child(field_name).set(field_value)
+            return redirect('Tourister:home')
+    else:
+        form = DataForm()
+
+    firebase_data = database.get().val()
+
+    return render(request, 'tourister/home.html', {
+        'form': form,
+        'firebase_data': firebase_data
+    })
