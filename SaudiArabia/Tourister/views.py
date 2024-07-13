a66056e0cadd5d9f8ac2d651a637258aedb6206b
e7gx <abalgabo@gmail.com>
2024-07-13 03:28:23 +0300

    setting up the ENV,APP👋

---
+++ SaudiArabia/Tourister/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+
+def home(request:HttpResponse):
+    return render(request, 'tourister/home.html')
+
