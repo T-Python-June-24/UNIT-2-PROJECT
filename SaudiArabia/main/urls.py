41a4e3881487c4d1562c2c28e39fcabafff2da83
Abdulrahman-Alwagdani <167430546+Abdulrahman-Alwagdani@users.noreply.github.com>
2024-07-15 11:35:25 +0300

    Imported Bootstrap and implemented dark mode

--- SaudiArabia/main/urls.py
+++ SaudiArabia/main/urls.py
@@ -3,4 +3,6 @@ from . import views
 app_name = "main"
 urlpatterns = [
     path("", views.home_view, name="home_view"),
+    path("mode/light", views.light_view, name="light_view"),
+    path("mode/dark", views.dark_view, name="dark_view"),
 ]
--- SaudiArabia/main/views.py
+++ SaudiArabia/main/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 
 
 # Create your views here.
 def home_view(request: HttpRequest) -> HttpResponse:
     return render(request, 'main/home.html')
+
+
+def dark_view(request: HttpRequest) -> HttpResponse:
+    response = redirect("main:home_view")
+    response.set_cookie("mode", "dark", max_age=86400)
+    return response
+
+
+def light_view(request: HttpRequest) -> HttpResponse:
+    response = redirect("main:home_view")
+    response.set_cookie("mode", "dark", max_age=-86400)
+    return response
