f12ac751b72a91c4d4832bfb65158aa2c7558f41
AhmadAlhazmi75 <rregfhlty@gmail.com>
2024-07-15 13:15:29 +0300

    cleaned the code (a little bit)

--- saudi_arabia/urls.py
+++ saudi_arabia/urls.py
@@ -5,8 +5,8 @@ app_name = 'saudi_arabia'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('dark_mode/', views.dark_mode, name='dark_mode'),
-    path('light_mode/', views.light_mode, name='light_mode'),
+    path('themes/dark_mode/', views.dark_mode, name='dark_mode'),
+    path('themes/light_mode/', views.light_mode, name='light_mode'),
     path('history/', views.history, name='history'),
     path('cuisine/', views.cuisine, name='cuisine'),
     path('kings/', views.kings, name='kings'),
