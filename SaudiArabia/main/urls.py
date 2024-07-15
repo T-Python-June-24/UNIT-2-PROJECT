e31363354e448e29411fc30afed7898db731dab2
Abdulrahman-Alwagdani <167430546+Abdulrahman-Alwagdani@users.noreply.github.com>
2024-07-15 20:55:22 +0300

    Completed HTML structure

--- SaudiArabia/main/urls.py
+++ SaudiArabia/main/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+
 app_name = "main"
 urlpatterns = [
     path("", views.home_view, name="home_view"),
+    path("games/", views.home_view, name="games_view"),
+    path("events/", views.home_view, name="events_view"),
+    path("teams/", views.home_view, name="teams_view"),
+    path("infrastructure/", views.home_view, name="infrastructure_view"),
     path("mode/light", views.light_view, name="light_view"),
     path("mode/dark", views.dark_view, name="dark_view"),
 ]
--- SaudiArabia/main/views.py
+++ SaudiArabia/main/views.py
@@ -7,6 +7,22 @@ def home_view(request: HttpRequest) -> HttpResponse:
     return render(request, 'main/home.html')
 
 
+def games_view(request: HttpRequest) -> HttpResponse:
+    return render(request, 'main/games.html')
+
+
+def events_view(request: HttpRequest) -> HttpResponse:
+    return render(request, 'main/events.html')
+
+
+def teams_view(request: HttpRequest) -> HttpResponse:
+    return render(request, 'main/teams.html')
+
+
+def infrastructure_view(request: HttpRequest) -> HttpResponse:
+    return render(request, 'main/infrastructure.html')
+
+
 def dark_view(request: HttpRequest) -> HttpResponse:
     response = redirect("main:home_view")
     response.set_cookie("mode", "dark", max_age=86400)
