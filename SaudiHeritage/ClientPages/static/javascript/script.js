2ab3af1d713a830f3ff2a854afb4764da2433b21
naif <naif.n1158@gmail.com>
2024-07-15 15:46:29 +0300

    Adding regions and adding modifications to the scrollbar have been completed

--- SaudiHeritage/ClientPages/static/javascript/script.js
+++ SaudiHeritage/ClientPages/static/javascript/script.js
@@ -1,11 +1,28 @@
-function updateBannerText() {
-    const inputText = document.getElementById('banner-text').value;
-    const animatedText = document.getElementById('animated-text');
-    animatedText.textContent = inputText;
 
-    
-    const textBanner = document.querySelector('.text-banner');
-    textBanner.style.animation = 'none';
-    textBanner.offsetHeight; 
-    textBanner.style.animation = '';
-}
+  $(document).ready(function() {
+    var counted = false;
+    $(window).scroll(function() {
+      var oTop = $('.nums').offset().top - window.innerHeight;
+      if (!counted && $(window).scrollTop() > oTop) {
+        $('.num').each(function() {
+          var $this = $(this),
+              countTo = $this.attr('data-goal');
+          $({ countNum: $this.text() }).animate({
+            countNum: countTo
+          },
+          {
+            duration: 2000,
+            easing: 'swing',
+            step: function() {
+              $this.text(Math.floor(this.countNum));
+            },
+            complete: function() {
+              $this.text(this.countNum);
+            }
+          });
+        });
+        counted = true;
+      }
+    });
+  });
+
