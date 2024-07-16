b78ba99eba7bb82bcfe25042e37e7a3c6fd2acd2
e7gx <abalgabo@gmail.com>
2024-07-16 11:37:48 +0300

    added a new animation for the time line-added new citys to the city cards -added a wiki link for every city in the properties list - adjust the content in the kings page- added new images for the citys 👍🏻

--- SaudiArabia/Tourister/views.py
+++ SaudiArabia/Tourister/views.py
@@ -8,7 +8,6 @@ from dotenv import load_dotenv
 import pandas as pd
 from langchain_openai import ChatOpenAI
 from langchain_experimental.agents.agent_toolkits import create_pandas_dataframe_agent
-from langchain.schema import HumanMessage, SystemMessage
 
 load_dotenv()  
 openai_key = os.getenv("OPENAI_API_KEY")
@@ -36,10 +35,6 @@ ONLY USE THE RESULTS OF THE CALCULATIONS YOU HAVE DONE**.
 
 """
 
-# - **ALWAYS**, as part of your "Final Answer", explain how you got
-# to the answer on a section that starts with: "\n\nExplanation:\n".
-# In the explanation, mention the column names that you used to get
-# to the final answer.
 
 
 properties = [
@@ -47,7 +42,11 @@ properties = [
     {"title": "Makkah","content": "Discover Mecca, Islam's holiest city and birthplace of Prophet Muhammad. With a population of 2.39 million in 2022, it is where the Quran was first revealed. Experience the spiritual essence of Mecca, home to the sacred Masjid al-Haram, the holiest mosque in Islam. offering a rich blend of spiritual and historical experiences.", "image": "makkah.jpg","link":"https://en.wikipedia.org/wiki/Mecca"},
     {"title": "Madinah","content": "Medina, or Al-Madinah al-Munawwarah, is a city of profound Islamic heritage, centered around Al-Masjid al-Nabawi, the mosque built by Prophet Muhammad. It's a key pilgrimage site with historical landmarks like Mount Uhud and the Quba Mosque, offering a rich blend of spiritual and historical experiences.", "image": "madinah.jpg","link":"https://en.wikipedia.org/wiki/Mecca"},
     {"title": "Jeddah","content": "Discover Jeddah, Saudi Arabia's bustling port city nestled along the Red Sea coast. Serving as the gateway to Mecca and Medina, Jeddah boasts a vibrant population of about 3.75 million. Renowned for its commercial significance, rich seafood culture, and popularity as a resort destination, Jeddah offers a vibrant blend of commerce and leisure", "image": "jed.jpg","link":"https://en.wikipedia.org/wiki/Jeddah"},
+    {"title": "Dammam","content": "Dammam, a vibrant port city in Saudi Arabia's Eastern Province, offers more than just its famous port. Enjoy the serene Arabian Gulf views, stroll along the Dammam Corniche, or visit the lush King Fahad Park, one of Saudi's largest. Explore Al Marjan Island, Saudi's first man-made island, or relax at Half Moon Bay. The city is becoming a hub for entertainment, with options ranging from movies at Ithra in Dhahran to shopping at Othaim Mall or the Dammam Souq, where local crafts and jewelry abound.", "image":"dammam.jpg","link":"https://en.wikipedia.org/wiki/Dammam"},
     {"title": "Al-Bahah","content": "Explore Al Baha, nestled in the picturesque Sarawat Mountains of southwestern Saudi Arabia. With its delightful climate and lush surrounding forests, Al Baha beckons as a premier tourist destination. Serving as the capital of the Al Bahah Region, it offers a serene retreat amidst natural beauty.", "image":"bahah.jpeg","link":"https://en.wikipedia.org/wiki/Al_Bahah"},
+    {"title": "Taif","content": "Known as the City of Roses, Taif is famed for its fragrant roses and perfume factories. As Saudi's summer retreat, it offers a cool escape with highlights like the Souq Okaz festival and the Crown Prince Camel Festival in August. Key attractions include Al Hada Mountain, with its long cable car ride, rose fields, and trails, plus the Al Kar Tourist Village featuring a water park and toboggan slide. Various hotels and resorts reflect Taif's unique charm.", "image":"taif.jpg","link":"https://en.wikipedia.org/wiki/Taif"},
+    {"title": "Qassim","content": "Located in Saudi’s central highlands, Qassim celebrates each year with the Buraydah Date Festival at the end of the harvest season. you should check out the Buraydah Museum for a glimpse into the history and culture of the region. Northwest of Buraydah lies the ancient oasis of Uyun Al Jiwa, which well-known Arab poets have cited in their work. At Al Bassam Heritage House, admire the many beautiful decorative patterns adorning this 3,500-square-meter Najdi-style palace.", "image":"qassim.jpg","link":"https://en.wikipedia.org/wiki/Al-Qassim_Province"},
+
 ]
 context = {'properties': properties,}
 
