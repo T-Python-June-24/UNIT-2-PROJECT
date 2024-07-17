ede7d527cb5916d2f39a89cc6ce294f1ea882c50
e7gx <abalgabo@gmail.com>
2024-07-17 22:00:36 +0300

    edit some of the content -added animation

--- SaudiArabia/Tourister/urls.py
+++ SaudiArabia/Tourister/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('home_light/', views.home_light, name='home_light'),
     path('home_dark/', views.home_dark, name='home_dark'),
     path('kings/', views.kings, name='kings'),
-    path('city/', views.city, name='city'),
+    path('citys/', views.city, name='city'),
     path('contact/', views.contact, name='contact'),
-    path('card/', views.card, name='card'),
+    path('Reviews/', views.card, name='card'),
     path('chatbot/', views.agent, name='chatbot'),
 
 ]
