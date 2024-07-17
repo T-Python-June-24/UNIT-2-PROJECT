a84458498adebbdb18293dd8aefa2da4a9da82bc
Abdulrahman-Alwagdani <167430546+Abdulrahman-Alwagdani@users.noreply.github.com>
2024-07-17 09:19:46 +0300

    Completed the content of the website. Only styling remains.

--- SaudiArabia/main/urls.py
+++ SaudiArabia/main/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = "main"
 urlpatterns = [
     path("", views.home_view, name="home_view"),
-    path("games/", views.home_view, name="games_view"),
-    path("events/", views.home_view, name="events_view"),
-    path("teams/", views.home_view, name="teams_view"),
-    path("infrastructure/", views.home_view, name="infrastructure_view"),
+    path("games/", views.games_view, name="games_view"),
+    path("events/", views.events_view, name="events_view"),
+    path("teams/", views.teams_view, name="teams_view"),
+    path("infrastructure/", views.infrastructure_view, name="infrastructure_view"),
     path("mode/light", views.light_view, name="light_view"),
     path("mode/dark", views.dark_view, name="dark_view"),
 ]
