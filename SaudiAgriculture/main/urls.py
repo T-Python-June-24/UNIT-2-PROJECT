1999154a5aebafefa3acc78e3a27852d94e3243e
Abdulaziz Asiri <aboodi-75-@hotmail.com>
2024-07-18 09:58:09 +0300

    final touch

--- SaudiAgriculture/main/urls.py
+++ SaudiAgriculture/main/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path("", views.home_view, name="home_view"),
     path("about/", views.about_view, name="about_view"),
     path("cities/", views.cities_view, name="cities_view"),
-    path("agriculture-technologies/", views.agriculture_technologies_view, name="agriculture_technologies_view"),
+    path("agriculture-technologies/", views.agriculture_technologies_view, name="agriculture_technologies_view" ),
+     path("dark/dark-mode/", views.dark_mode_view, name="dark_mode_view"),
+    path("light/light-mode/", views.light_mode_view, name="light_mode_view"),
 ]
--- SaudiAgriculture/main/views.py
+++ SaudiAgriculture/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest,HttpResponse
 
 def home_view(request: HttpRequest):
@@ -13,6 +13,20 @@ def cities_view(request: HttpRequest):
 
 def agriculture_technologies_view(request: HttpRequest):
     return render(request, "agricultureTechnologies.html")
+
+def dark_mode_view(request: HttpRequest):
+
+    response = redirect("main:home_view")
+    response.set_cookie("dark", "dark-mode")
+    return response
+
+
+def light_mode_view(request: HttpRequest):
+
+    response = redirect("home_view")
+    response.set_cookie("light", "light-mode")
+    return response
+
 
 
 
